7cc86836482d4032d4ab6f555dce8fefcee96c2d
Radomír Žemlička <info@razem.cz>
2015-07-22 18:09:00 +0200

    Test comment fix

--- test/index.js
+++ test/index.js
@@ -4,9 +4,9 @@
  * How to test:
  * 1. npm install
  * 2. Install Apache & PHP
- * 3. Set this directory as http://localhost/legio-request/test
+ * 3. Set this directory as http://localhost/app/reqio/test
  * 4. npm test
- * 5. http://localhost/legio-request/test
+ * 5. http://localhost/app/reqio/test
  */
 
 require("./file");
